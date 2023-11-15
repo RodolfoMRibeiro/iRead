160ffae9b6a13998f1b3a1e2eb60d856642d6352
rodolfo marques ribeiro <rodolfomarqribeiro@gmail.com>
2023-11-15 15:19:48 -0300

    feat: use auth provider and load dependencies

--- App.tsx
+++ App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { X } from './src/components/Header';
+import { AuthProvider } from './src/contexts/Auth';
+import { Router } from './src/routes/Router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>looser</Text>
-      <StatusBar style="auto" />
-      <X />
-    </View>
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
