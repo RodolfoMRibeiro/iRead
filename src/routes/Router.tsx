9dbac19d49d7c3359e85e42b126b4b5d77f8962c
rodolfo marques ribeiro <rodolfomarqribeiro@gmail.com>
2023-11-15 15:13:55 -0300

    feat: add simple loading wait message to router

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,15 +1,24 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import { View, Text } from 'react-native';
 import { AppStack } from './AppStack';
 import { AuthStack } from './AuthStack';
 import { useAuth } from '../contexts/Auth';
 
 export function Router() {
-  const { auth } = useAuth();
+  const { authData, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Carregando...</Text>
+      </View>
+    );
+  }
 
   return (
     <NavigationContainer>
-      {auth ? <AppStack /> : <AuthStack />}
+      {authData ? <AppStack /> : <AuthStack />}
     </NavigationContainer>
   );
 }
